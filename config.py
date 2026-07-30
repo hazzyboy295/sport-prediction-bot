5a0ff81b3f77c6a1323ce74c850e0e3a22267126
hazzyboy295 <antonychris295@gmail.com>
2026-07-30 19:31:17 +0100

    Add Telegram bot configuration to config.py
    Added configuration settings for Telegram bot.

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+# Your Telegram ID
+ADMIN_ID = 8716632096
+
+# Bot token from Railway Variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Change these later
+CHANNEL_USERNAME = "@YourChannelUsername"
+ADMIN_USERNAME = "@YourTelegramUsername"
